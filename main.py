d6790bfd07e24c354910c19716b6f28b7cf6e942
jordan <oaflopean@gmail.com>
2018-03-16 22:15:31 -0500

    Basic app

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
+from forms import LoginForm
+from config import Config
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html',
+                           title='Home',
+                           user=user, posts=posts)
+
+# ...
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/form')
 def form():
     return render_template('form.html')
+
 @app.route('/submitted', methods=['POST'])
 def submitted_form():
     name = request.form['name']
